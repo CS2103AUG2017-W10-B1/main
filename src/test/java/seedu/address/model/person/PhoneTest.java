fddb525e4a60ca70163b7bd7225a8bc6fe8f8254
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-10-09 19:54:45 +0800

    allow spaces, dashes, and parentheses in phone numbers

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -15,11 +15,14 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
         assertTrue(Phone.isValidPhone("93121534"));
         assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+        assertTrue(Phone.isValidPhone("9312 1534")); // spaces within digits
+        assertTrue(Phone.isValidPhone("9312-1534")); // dashes within digits
+        assertTrue(Phone.isValidPhone("(9312) 1534")); // parentheses within digits
+        assertTrue(Phone.isValidPhone("(123) 456-7890")); // complex phone number
     }
 }
