061d805aff3a199ad3fffb275fc2b2e3a3608de6
namjjd <namjjd@gmail.com>
2017-10-16 22:30:55 +0800

    add javadoc comment

--- src/main/java/seedu/address/model/person/Created.java
+++ src/main/java/seedu/address/model/person/Created.java
@@ -1,17 +1,27 @@
 package seedu.address.model.person;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static java.util.Objects.requireNonNull;
 
+/*
+ * Represent's the timestamp when the Person object was created
+ * Guarantees: immutable
+ */
 public class Created {
 
-    public final Date createdAt;
+    public static final String MESSAGE_CREATED_CONSTRAINTS =
+            "Person created (at) will only take a Date object";
+
+    public final String createdAt;
 
     public Created(Date date) {
         requireNonNull(date);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        sdf.setLenient(false);
 
-        this.createdAt = date;
+        this.createdAt = sdf.format(date);
     }
 
     @Override
