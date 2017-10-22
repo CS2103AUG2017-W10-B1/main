a35b70d2445ac0c2b6621dd215273fdcaeefdcc1
namjjd <namjjd@gmail.com>
2017-10-22 21:22:26 +0800

    fix linter complaint

--- src/main/java/seedu/address/logic/commands/StatisticsCommand.java
+++ src/main/java/seedu/address/logic/commands/StatisticsCommand.java
@@ -5,7 +5,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 /**
  * Displays the statistics of the address book to the user.
  */
-public class StatisticsCommand extends Command{
+public class StatisticsCommand extends Command {
     public static final String COMMAND_WORD = "statistics";
     public static final String COMMAND_ALIAS = "st";
     public static final String MESSAGE_USAGE = COMMAND_WORD
