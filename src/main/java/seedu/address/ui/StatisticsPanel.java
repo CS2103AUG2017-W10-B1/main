8f40bf535d276fddb61467ed7bdbb71b83358ffd
namjjd <namjjd@gmail.com>
2017-10-22 20:19:17 +0800

    add javadoc comment

--- src/main/java/seedu/address/ui/StatisticsPanel.java
+++ src/main/java/seedu/address/ui/StatisticsPanel.java
@@ -3,11 +3,11 @@ package seedu.address.ui;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
 
-import java.util.logging.Logger;
-
-public class StatisticsPanel extends UiPart<Region>{
+/**
+ * The Statistics Panel of the App.
+ */
+public class StatisticsPanel extends UiPart<Region> {
     private static final String FXML = "StatisticsPanel.fxml";
 
     @FXML
