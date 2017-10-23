57131a4b1a0ae0837876b0b4cbf1be9c110f942f
namjjd <namjjd@gmail.com>
2017-10-23 14:34:39 +0800

    update changes to statistics panel. StatisticsPanel must now accept an ObservableList of ReadOnlyPerson

--- src/main/java/seedu/address/ui/StatisticsPanel.java
+++ src/main/java/seedu/address/ui/StatisticsPanel.java
@@ -1,8 +1,10 @@
 package seedu.address.ui;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Region;
+import seedu.address.model.person.ReadOnlyPerson;
 
 /**
  * The Statistics Panel of the App.
@@ -13,7 +15,7 @@ public class StatisticsPanel extends UiPart<Region> {
     @FXML
     private PieChart pieChart;
 
-    public StatisticsPanel() {
+    public StatisticsPanel(ObservableList<ReadOnlyPerson> list) {
         super(FXML);
     }
 }
