1ee72760c7cec8afc9575ffa24f1e3fd4d0764fe
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-10-24 23:48:46 +0800

    Add tests

---
+++ src/test/java/seedu/address/logic/commands/SizeCommandTest.java
@@ -0,0 +1,119 @@
+package seedu.address.logic.commands;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.FontSizeChangeRequestEvent;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code SizeCommand}.
+ */
+public class SizeCommandTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    private Model model;
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_resetSize() {
+        SizeCommand sizeCommand = prepareCommand();
+        CommandResult commandResult = sizeCommand.execute();
+        assertEquals(SizeCommand.MESSAGE_RESET_FONT_SUCCESS, commandResult.feedbackToUser);
+
+        FontSizeChangeRequestEvent lastEvent =
+                (FontSizeChangeRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertTrue(lastEvent.isReset);
+    }
+
+    @Test
+    public void execute_increaseSize() {
+        SizeCommand sizeCommand = prepareCommand(3);
+        CommandResult commandResult = sizeCommand.execute();
+        assertEquals(String.format(SizeCommand.MESSAGE_CHANGE_FONT_SUCCESS, "increment", 3),
+                commandResult.feedbackToUser);
+
+        FontSizeChangeRequestEvent lastEvent =
+                (FontSizeChangeRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertFalse(lastEvent.isReset);
+        assertEquals(3, lastEvent.sizeChange);
+    }
+
+    @Test
+    public void execute_decreaseSize() {
+        SizeCommand sizeCommand = prepareCommand(-3);
+        CommandResult commandResult = sizeCommand.execute();
+        assertEquals(String.format(SizeCommand.MESSAGE_CHANGE_FONT_SUCCESS, "decrement", 3),
+                     commandResult.feedbackToUser);
+
+        FontSizeChangeRequestEvent lastEvent =
+                (FontSizeChangeRequestEvent) eventsCollectorRule.eventsCollector.getMostRecent();
+        assertFalse(lastEvent.isReset);
+        assertEquals(-3, lastEvent.sizeChange);
+    }
+
+    @Test
+    public void equals() {
+        SizeCommand sizeResetCommand = new SizeCommand();
+        SizeCommand sizeIncrementCommand = new SizeCommand(1);
+        SizeCommand sizeDecrementCommand = new SizeCommand(1);
+
+        // same object -> returns true
+        assertTrue(sizeResetCommand.equals(sizeResetCommand));
+
+        // same values -> returns true
+        SizeCommand sizeResetCommandCopy = new SizeCommand();
+        assertTrue(sizeResetCommand.equals(sizeResetCommandCopy));
+
+        // different types -> returns false
+        assertFalse(sizeResetCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(sizeResetCommand.equals(null));
+
+        // different type -> returns false
+        assertFalse(sizeResetCommand.equals(sizeIncrementCommand));
+
+        // different value -> returns false
+        assertFalse(sizeDecrementCommand.equals(sizeIncrementCommand));
+    }
+
+    /**
+     * Returns a {@code SizeCommand} with parameters given.
+     */
+    private SizeCommand prepareCommand() {
+        SizeCommand sizeCommand = new SizeCommand();
+        sizeCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return sizeCommand;
+    }
+
+    private SizeCommand prepareCommand(int change) {
+        SizeCommand sizeCommand = new SizeCommand(change);
+        sizeCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return sizeCommand;
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/SizeCommandParserTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+import seedu.address.logic.commands.SizeCommand;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+/**
+ * Test scope: similar to {@code DeleteCommandParserTest}.
+ * @see DeleteCommandParserTest
+ */
+public class SizeCommandParserTest {
+
+    private SizeCommandParser parser = new SizeCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSizeCommand() {
+        assertParseSuccess(parser, "1", new SizeCommand(1));
+        assertParseSuccess(parser, "-1", new SizeCommand(-1));
+        assertParseSuccess(parser, "", new SizeCommand());
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SizeCommand.MESSAGE_USAGE));
+    }
+}
