3ed78624de9aece65dc6462a8da4530aadf3e53d
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-11-10 18:15:07 +0800

    Fix tests

---
+++ src/test/java/seedu/address/ui/testutil/LogicStub.java
@@ -0,0 +1,59 @@
+package seedu.address.ui.testutil;
+
+import javafx.collections.ObservableList;
+import seedu.address.logic.ListElementPointer;
+import seedu.address.logic.Logic;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.ReadOnlyPerson;
+
+import static org.junit.Assert.fail;
+
+public class LogicStub implements Logic {
+
+    // @@author donjar
+
+    private ObservableList<ReadOnlyPerson> persons;
+
+    public LogicStub() {
+        this.persons = null;
+    }
+
+    public LogicStub(ObservableList<ReadOnlyPerson> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public CommandResult execute(String commandText) throws CommandException, ParseException {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    @Override
+    public ObservableList<ReadOnlyPerson> getAllPersonList() {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    @Override
+    public ObservableList<ReadOnlyPerson> getFilteredPersonList() {
+        if (persons == null) {
+            fail("This method should not be called.");
+        }
+
+        return persons;
+    }
+
+    @Override
+    public ListElementPointer getHistorySnapshot() {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    @Override
+    public int getFontSizeChange() {
+        fail("This method should not be called.");
+        return 0;
+    }
+}
