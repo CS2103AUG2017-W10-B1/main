254ad3aa3d376bf29482bdb1ac34f1bf4ac79de8
namjjd <namjjd@gmail.com>
2017-11-11 19:52:27 +0800

    fix linter errors

--- src/test/java/seedu/address/logic/commands/StatisticsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatisticsCommandTest.java
@@ -1,14 +1,14 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.StatisticsCommand.MESSAGE_SUCCESS;
+
 import org.junit.Rule;
 import org.junit.Test;
 import seedu.address.commons.events.ui.ToggleStatisticsPanelEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.ui.testutil.EventsCollectorRule;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.StatisticsCommand.MESSAGE_SUCCESS;
 
 //@@author 500poundbear
 public class StatisticsCommandTest {
--- src/test/java/seedu/address/model/person/RemarkTest.java
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import seedu.address.model.person.Remark;
-
 public class RemarkTest {
 
     //@@author 500poundbear
